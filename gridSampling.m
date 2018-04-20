64a22901affd5f70c4a78d555d9a9c994aa35071
Fengxue Zhang <zhangfx7@mail2.sysu.edu.cn>
2018-04-21 01:24:56 +0800

    decrease sampling count

--- gridSampling.m
+++ gridSampling.m
@@ -1,16 +1,17 @@
 % moving the sampling area by grid to try to get eoungh sample of positive area and false area
 clc,clear;
+targetSize = 224;
 totalCount = 0;
 sampleAmount = 5;
-sampleSize = [80, 80];
+sampleSize = [224, 224];
 sampleAreaSize = sampleSize(1) * sampleSize(2) * 255;
 halfHeight = sampleSize(1)/2;
 halfLength = sampleSize(2)/2;
 % folderName = 'F:\Dropbox\USSeg2013\US2DImgs\';
 % ---------define positions----------
-folderName = '/Users/fengxuezhang/Documents/Lab/US2DImgs/';  % folders of the origin BUSimages
-negativeFolderName = '/Users/fengxuezhang/Documents/Lab/CancerDetectionImgs/negative2/'; % folder to store the negative region
-positiveFolderName = '/Users/fengxuezhang/Documents/Lab/CancerDetectionImgs/positive2/'; % folder to store the positive region
+folderName = 'E:\中山大学\大三\LAB\Breast Cancer\US2DImgs\';  % folders of the origin BUSimages
+negativeFolderName = 'E:\中山大学\大三\LAB\Breast Cancer\2017-2018春季学期\falsePositiveDetection\CancerDetectionImgs\CancerDetectionImgs\negative\'; % folder to store the negative region
+positiveFolderName = 'E:\中山大学\大三\LAB\Breast Cancer\2017-2018春季学期\falsePositiveDetection\CancerDetectionImgs\CancerDetectionImgs\positive\'; % folder to store the positive region
 
 % ----------create folders-----------
 mkdir(negativeFolderName);
@@ -22,14 +23,14 @@ caseEndNum = 115;
 for i = caseStartNum:caseEndNum
     sprintf('case %d',i)
     %%% directing to origin BUSimages as /*b_2Value.jpg & /*a.jpg
-    goldenStdName = sprintf('%s%d%s',folderName,i,'/*b_2Value.jpg');
+    goldenStdName = sprintf('%s%d%s',folderName,i,'\*b_2Value.jpg');
     goldenStdBUSFiles = dir(goldenStdName);
     for j = 1:size(goldenStdBUSFiles)
         goldenStdImgName = goldenStdBUSFiles(j).name;
         imgNum = goldenStdImgName(1);
         % sampling
-        goldenStdImageName = sprintf('%s%d%c%s',folderName,i,'/',goldenStdImgName);
-        originImageName = sprintf('%s%d%c%s',folderName,i,'/',imgNum,'a.jpg');
+        goldenStdImageName = sprintf('%s%d%c%s',folderName,i,'\',goldenStdImgName);
+        originImageName = sprintf('%s%d%c%s',folderName,i,'\',imgNum,'a.jpg');
         if exist(goldenStdImageName,'file') == 0 
             continue;
         end
@@ -63,21 +64,21 @@ for i = caseStartNum:caseEndNum
                 sampledAreaOrg = org(CenterY - halfHeight:CenterY + halfHeight - 1, ...
                     CenterX - halfLength: CenterX + halfLength - 1);
                 areaSum = sum(sum(sampledAreaGld));
-    %             sampleAreaSize * 0.1
-    %             imshow(sampledAreaGld)
-    %             pause()
+
                 %judge and save
                 if sampleNegativeCount <= 2 * sampleAmount && areaSum <= sampleAreaSize * 0.1
                     svSampleImgName = sprintf('%s%d%s',negativeFolderName,totalCount,'.jpg');
                     sampleNegativeCount = sampleNegativeCount + 1;
-                elseif areaSum >= sampleAreaSize * 0.5
+                elseif areaSum >= sampleAreaSize * 0.7
                     svSampleImgName = sprintf('%s%d%s',positiveFolderName,totalCount,'.jpg');
                     sampleCount = sampleCount + 1;
                 else
-                    CenterY = CenterY + halfHeight
+                    CenterY = CenterY + halfHeight;
                     continue;
                 end
                 totalCount = totalCount + 1;
+                sampledAreaOrg = imresize(sampledAreaOrg, [targetSize, targetSize]);
+                sampledAreaOrg = gray2rgb(sampledAreaOrg);
                 imwrite(sampledAreaOrg, svSampleImgName);
                 CenterY = CenterY + halfHeight;
             end
